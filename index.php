a1b547a883de874d57a7a48e19f3c8749d955d3d
hannabeee <158136329+hannabeee@users.noreply.github.com>
2024-05-22 14:29:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CPIS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+ 
+      
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+<div>
+    <div class="container">
+                <div class="loginHeader">
+                    <h1>NwSSU</h1>
+                    <p>Computer Peripheral Inventory System</p>
+                </div>
+    <div class="wrapper">
+    <img src="325672730_961885291443586_1156869176267253069_n.jpg "  alt="Northwest Samar State University" class="logo" style="display: block; margin: 0 auto; width: 150px; height: auto;">
+    
+
+   <h2>Admin</h2>
+        <form action="#" method="post">
+            <div class="input-field">
+                <input type="email" id="email" name="email" placeholder="Username" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-field">
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <i class='bx bxs-lock-alt'></i>
+                
+            </div>
+            <input type="submit" class="login" name="login" placeholder="Submit" required>
+             <a href="login.php">Student's Login</a>
+        </form>
+    </div>
+  
+</body>
+</html>
